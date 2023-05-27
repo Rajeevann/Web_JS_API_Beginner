c58a6ce21f3e3b1e110e158810c09a746df796ea
Rajeevan <rajee.srs@gmail.com>
2023-05-26 22:23:25 -0700

    js script update

--- Dictionary App/index.js
+++ Dictionary App/index.js
@@ -2,9 +2,10 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
+const input = document.getElementById("inp-word");
 
 const x = () => {
-  let inpWord = document.getElementById("inp-word").value;
+  let inpWord = input.value;
   fetch(`${url}${inpWord}`)
     .then((res) => res.json())
     .then((data) => {
@@ -39,3 +40,77 @@ function playSound() {
 }
 
 btn.addEventListener("click", x);
+
+input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    x();
+  }
+});
+
+/*
+
+// Assigning variables
+
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
+
+
+// This declares a function x, which is the event handler for the search button.
+
+const x = () => {
+  let inpWord = document.getElementById("inp-word").value;
+  fetch(`${url}${inpWord}`)
+    .then((res) => res.json())
+    .then((data) => { result.textContent = "";
+
+
+//These lines create a new div element with the class "word" and append it to the result element. Inside the div, a h3 element is created to display the entered word, and a "Play Sound" button is created and appended. An event listener is added to the button to call the playSound function when clicked.
+
+const wordDiv = document.createElement("div");
+wordDiv.classList.add("word");
+
+const wordHeading = document.createElement("h3");
+wordHeading.textContent = inpWord;
+wordDiv.appendChild(wordHeading);
+
+const playButton = document.createElement("button");
+playButton.textContent = "Play Sound";
+playButton.addEventListener("click", playSound);
+wordDiv.appendChild(playButton);
+
+result.appendChild(wordDiv);
+
+//These lines create a new div element with the class "details" and append it to the result element. Inside the div, two p elements are created to display the part of speech and phonetic representation of the word. 
+
+const detailsDiv = document.createElement("div");
+detailsDiv.classList.add("details");
+
+const partOfSpeech = document.createElement("p");
+partOfSpeech.textContent = data[0].meanings[0].partOfSpeech;
+detailsDiv.appendChild(partOfSpeech);
+
+const phonetic = document.createElement("p");
+phonetic.textContent = `/${data[0].phonetic}/`;
+detailsDiv.appendChild(phonetic);
+
+result.appendChild(detailsDiv);
+
+//This creates a new p element with the class "word-meaning" to display the definition of the word. The definition is obtained from the API response and assigned to the textContent property of the element.
+
+      const meaningParagraph = document.createElement("p");
+      meaningParagraph.classList.add("word-meaning");
+      meaningParagraph.textContent = data[0].meanings[0].definitions[0].definition;
+      result.appendChild(meaningParagraph);
+
+//This checks if an example is available for the word in the API response. If an example exists, a new p element with the class "word-example" is created
+
+      if (data[0].meanings[0].definitions[0].example) {
+        const exampleParagraph = document.createElement("p");
+        exampleParagraph.classList.add("word-example");
+        exampleParagraph.textContent = data[0].meanings[0].definitions[0].example;
+        result.appendChild(exampleParagraph);
+      }
+*/
